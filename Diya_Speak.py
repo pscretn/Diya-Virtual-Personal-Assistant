fce24141efc483f2d256dbcf6950694fd821e7f9
PAUL SABU <paulsabu.123@gmail.com>
2022-03-20 23:12:40 +0530

    Update Diya_Speak.py

--- Diya_Speak.py
+++ Diya_Speak.py
@@ -4,7 +4,7 @@ def say(text):
     engine = pyt.init()
     voices = engine.getProperty('voices')
     rate = engine.getProperty('rate')
-    engine.setProperty('voice',voices[3].id)
+    engine.setProperty('voice',voices[1].id)
     engine.setProperty('rate',140)
     engine.say(text)
     engine.runAndWait()
